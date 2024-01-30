215af4a323734b24768a5c22e9ce6ac5b2b12387
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-30 11:18:13 +0000

    Tue, Jan 30, 2024, 11:18 AM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -36,3 +36,15 @@ BEGIN
     CALCULATE_TOTAL_PAYMENT;
 END;
 /
+
+
+
+DECLARE
+lv_taxrate_num CONSTANT NUMBER(2,2) := .06;
+lv_total_num NUMBER(6,2) := 50;
+lv_taxamt_num NUMBER(4,2);
+BEGIN
+lv_taxamt_num := lv_total_num * lv_taxrate_num;
+DBMS_OUTPUT.PUT_LINE(lv_taxamt_num);
+END;
+/
