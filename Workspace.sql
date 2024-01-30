5cd3e422d8575aadadb995433f0283ebd06c5465
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-30 11:20:17 +0000

    Update workspace

--- Workspace.sql
+++ Workspace.sql
@@ -37,14 +37,12 @@ BEGIN
 END;
 /
 
-
-
 DECLARE
-lv_taxrate_num CONSTANT NUMBER(2,2) := .06;
-lv_total_num NUMBER(6,2) := 50;
-lv_taxamt_num NUMBER(4,2);
+    LV_TAXRATE_NUM CONSTANT NUMBER(2, 2) := .06;
+    LV_TOTAL_NUM   NUMBER(6, 2) := 50;
+    LV_TAXAMT_NUM  NUMBER(4, 2);
 BEGIN
-lv_taxamt_num := lv_total_num * lv_taxrate_num;
-DBMS_OUTPUT.PUT_LINE(lv_taxamt_num);
+    LV_TAXAMT_NUM := LV_TOTAL_NUM * LV_TAXRATE_NUM;
+    DBMS_OUTPUT.PUT_LINE(LV_TAXAMT_NUM);
 END;
 /
