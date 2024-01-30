803737ad3944e246c694c46a6763ab62608c90d6
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-30 11:21:57 +0000

    Update workspace

--- Workspace.sql
+++ Workspace.sql
@@ -46,3 +46,59 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(LV_TAXAMT_NUM);
 END;
 /
+
+DECLARE
+    MESSAGE VARCHAR2(50) := 'Hello, World!';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(MESSAGE);
+END;
+/
+
+-- Scalar Variables
+DECLARE
+    MYNUMBER  INTEGER := 10;
+    MYSTRING  VARCHAR2(20) := 'PL/SQL Example';
+    MYBOOLEAN BOOLEAN := TRUE;
+BEGIN
+ -- Using IF Statements
+    IF MYNUMBER > 5 THEN
+        DBMS_OUTPUT.PUT_LINE('The number is greater than 5.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('The number is 5 or less.');
+    END IF;
+ -- Using CASE Statements
+    CASE
+        WHEN MYSTRING = 'PL/SQL Example' THEN
+            DBMS_OUTPUT.PUT_LINE('The string matches.');
+        WHEN MYSTRING = 'Another Example' THEN
+            DBMS_OUTPUT.PUT_LINE('This is a different example.');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('No match found.');
+    END CASE;
+ -- Using a Boolean Variable
+    IF MYBOOLEAN THEN
+        DBMS_OUTPUT.PUT_LINE('The boolean variable is TRUE.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('The boolean variable is FALSE.');
+    END IF;
+ -- Using Looping Statements
+ -- Creating a Flowchart (Textual Representation)
+ -- [Start] --> [Loop Start] --> [IF condition] --> [Do something] --> [Loop End] --> [End]
+ -- Using a FOR Loop
+    FOR I IN 1..5 LOOP
+        DBMS_OUTPUT.PUT_LINE('FOR Loop Iteration: '
+                             || I);
+    END LOOP;
+ -- Using a Basic Loop
+    DECLARE
+        LOOPCOUNTER INTEGER := 0;
+    BEGIN
+        LOOP
+            LOOPCOUNTER := LOOPCOUNTER + 1;
+            EXIT WHEN LOOPCOUNTER > 3;
+            DBMS_OUTPUT.PUT_LINE('Basic Loop Iteration: '
+                                 || LOOPCOUNTER);
+        END LOOP;
+    END;
+END;
+/
