81fca8a6e1aeb0ba9aaafe181f1bdbfe760a5f57
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-05 12:00:35 +0000

    Mon, Feb 5, 2024, 12:00 PM +00:00

--- assignment_03.sql
+++ assignment_03.sql
@@ -1,9 +1,8 @@
 -- Assignment 2-3: Using IF Statements
 -- Create a block using an IF statement to perform the actions described in Assignment 2-2. Use a scalar variable for the total purchase amount, and initialize this variable to different values to test your block.
 DECLARE
-    lv_total_purchases NUMBER;
+    lv_total_purchases NUMBER := 150;
 BEGIN
-    lv_total_purchases := 150;
     DBMS_OUTPUT.PUT_LINE('Enter Total Purchases: ' || lv_total_purchases);
 
     IF lv_total_purchases > 200 THEN
--- assignment_04.sql
+++ assignment_04.sql
@@ -2,16 +2,14 @@
 -- Create a block using a CASE statement to perform the actions described in Assignment 2-2. Use a scalar variable for the total purchase amount, and initialize this variable to different values to test your block.
 DECLARE
     lv_total_purchases NUMBER := 150;
-    lv_high_limit NUMBER := 200;
-    lv_mid_limit NUMBER := 100;
 BEGIN
     DBMS_OUTPUT.PUT_LINE('Enter Total Purchases: ' || lv_total_purchases);
 
     CASE
-        WHEN lv_total_purchases > lv_high_limit THEN
+        WHEN lv_total_purchases > 200 THEN
             DBMS_OUTPUT.PUT_LINE('Customer Rated High');
             DBMS_OUTPUT.PUT_LINE('Display "High Rating"');
-        WHEN lv_total_purchases > lv_mid_limit THEN
+        WHEN lv_total_purchases > 100 THEN
             DBMS_OUTPUT.PUT_LINE('Customer Rated Mid');
             DBMS_OUTPUT.PUT_LINE('Display "Mid Rating"');
         ELSE
