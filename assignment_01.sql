a880253ed9394245082e918b295a258cee812ba8
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-05 11:56:39 +0000

    Mon, Feb 5, 2024, 11:56 AM +00:00

--- assignment_01.sql
+++ assignment_01.sql
@@ -1,16 +1,13 @@
 -- Assignment 2-1: Using Scalar Variables
 -- Create a PL/SQL block containing the following variables: Name Data Type Option Initial Value lv_test_date DATE December 10, 2012 lv_test_num NUMBER(3) CONSTANT 10 lv_test_txt VARCHAR2(10) Assign your last name as the value of the text variable in the executable section of the block. Include statements in the block to display each variable’s value onscreen.
 DECLARE
-    LV_TEST_DATE DATE := TO_DATE('10-DEC-2012', 'DD-MON-YYYY');
-    LV_TEST_NUM  CONSTANT NUMBER(3) := 10;
-    LV_TEST_TXT  VARCHAR2(10);
+    lv_test_date DATE := TO_DATE('10-DEC-2012', 'DD-MON-YYYY');
+    lv_test_num CONSTANT NUMBER(3) := 10;
+    lv_test_txt VARCHAR2(10);
 BEGIN
-    LV_TEST_TXT := 'Copilot';
-    DBMS_OUTPUT.PUT_LINE('Date: '
-                         || TO_CHAR(LV_TEST_DATE, 'DD-MON-YYYY'));
-    DBMS_OUTPUT.PUT_LINE('Number: '
-                         || LV_TEST_NUM);
-    DBMS_OUTPUT.PUT_LINE('Text: '
-                         || LV_TEST_TXT);
+    lv_test_txt := 'Copilot'; -- Assigning the last name
+    DBMS_OUTPUT.PUT_LINE('Date: ' || TO_CHAR(lv_test_date, 'DD-MON-YYYY'));
+    DBMS_OUTPUT.PUT_LINE('Number: ' || lv_test_num);
+    DBMS_OUTPUT.PUT_LINE('Text: ' || lv_test_txt);
 END;
 /
--- assignment_03.sql
+++ assignment_03.sql
@@ -1,16 +1,15 @@
 -- Assignment 2-3: Using IF Statements
 -- Create a block using an IF statement to perform the actions described in Assignment 2-2. Use a scalar variable for the total purchase amount, and initialize this variable to different values to test your block.
 DECLARE
-    lv_total_purchases NUMBER := 150;
-    lv_high_limit NUMBER := 200;
-    lv_mid_limit NUMBER := 100;
+    lv_total_purchases NUMBER;
 BEGIN
+    lv_total_purchases := 150;
     DBMS_OUTPUT.PUT_LINE('Enter Total Purchases: ' || lv_total_purchases);
 
-    IF lv_total_purchases > lv_high_limit THEN
+    IF lv_total_purchases > 200 THEN
         DBMS_OUTPUT.PUT_LINE('Customer Rated High');
         DBMS_OUTPUT.PUT_LINE('Display "High Rating"');
-    ELSIF lv_total_purchases > lv_mid_limit THEN
+    ELSIF lv_total_purchases > 100 THEN
         DBMS_OUTPUT.PUT_LINE('Customer Rated Mid');
         DBMS_OUTPUT.PUT_LINE('Display "Mid Rating"');
     ELSE
@@ -21,3 +20,4 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('End of Flowchart');
 END;
 /
+
