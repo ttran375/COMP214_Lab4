9093f3ed2ed7a9666609e7e0451b781535f05a82
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-05 13:56:32 +0000

    Mon, Feb 5, 2024, 1:56 PM +00:00

--- assignment_05.sql
+++ assignment_05.sql
@@ -1,9 +1,9 @@
 -- Assignment 2-5: Using a Boolean Variable
 -- Brewbean’s needs program code to indicate whether an amount is still due on an account when a payment is received. Create a PL/SQL block using a Boolean variable to indicate whether an amount is still due. Declare and initialize two variables to provide input for the account balance and the payment amount received. A TRUE Boolean value should indicate an amount is still owed, and a FALSE value should indicate the account is paid in full. Use output statements to confirm that the Boolean variable is working correctly.
 DECLARE
-    lv_account_balance NUMBER := 1000; -- Initialize with the account balance
-    lv_payment_received NUMBER := 500; -- Initialize with the payment amount received
-    lv_amount_due BOOLEAN; -- Boolean variable to indicate whether an amount is still due
+    lv_account_balance NUMBER := 1000;
+    lv_payment_received NUMBER := 500;
+    lv_amount_due BOOLEAN;
 BEGIN
     IF lv_payment_received < lv_account_balance THEN
         lv_amount_due := TRUE;
