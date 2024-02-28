3e145da9775262564c2e02725eee3f09dcc56b0f
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-28 11:26:27 +0000

    Wed, Feb 28, 2024, 11:26 AM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -9,40 +9,28 @@
 -- mix of roasted beans, a medium body', 'roasted.jpg',9.50,1).
 -- 3. Check whether the update was successful by querying the BB_PRODUCT table.
 
--- Step 1: Create the procedure
 CREATE OR REPLACE PROCEDURE PROD_ADD_SP (
-  p_product_name IN VARCHAR2,
-  p_description IN VARCHAR2,
-  p_image_filename IN VARCHAR2,
-  p_price IN NUMBER,
-  p_active_status IN NUMBER
-) AS
+    p_ProductName IN VARCHAR2,
+    p_Description IN VARCHAR2,
+    p_ProductImage IN VARCHAR2,
+    p_Price IN NUMBER,
+    p_Active IN NUMBER
+)
+IS
 BEGIN
-  INSERT INTO BB_PRODUCT (
-    product_name,
-    description,
-    image_filename,
-    price,
-    active_status
-  ) VALUES (
-    p_product_name,
-    p_description,
-    p_image_filename,
-    p_price,
-    p_active_status
-  );
-  COMMIT;
+    INSERT INTO BB_Product (idProduct, ProductName, Description, ProductImage, Price, Active)
+    VALUES (bb_prodid_seq.NEXTVAL, p_ProductName, p_Description, p_ProductImage, p_Price, p_Active);
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('New product added successfully.');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END PROD_ADD_SP;
+/
+
+BEGIN
+    PROD_ADD_SP('Roasted Blend', 'Well-balanced mix of roasted beans, a medium body', 'roasted.jpg', 9.50, 1);
 END;
 /
 
--- Step 2: Call the procedure with the specified parameter values
-BEGIN
-  PROD_ADD_SP('Roasted Blend', 'Well-balanced mix of roasted beans, a medium body', 'roasted.jpg', 9.50, 1);
-END;
-/
-
--- Step 3: Check whether the update was successful by querying the BB_PRODUCT table
-SELECT
-  *
-FROM
-  BB_PRODUCT;
+SELECT * FROM BB_Product;
