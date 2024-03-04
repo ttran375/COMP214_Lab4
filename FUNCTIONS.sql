056c7847b7047240e869a3cced268470c022f532
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-04 10:12:56 +0000

    Mon, Mar 4, 2024, 10:12 AM +00:00

--- FUNCTIONS.sql
+++ FUNCTIONS.sql
@@ -375,12 +375,72 @@ FROM
 -- to show its use with the following pledge information: pledge amount = $240 and monthly
 -- payments = 12. Also, use the function in an SQL statement that displays information for all
 -- donor pledges in the database on a monthly payment plan.
+CREATE OR REPLACE FUNCTION DD_MTHPAY_SF(
+  v_paymonths IN NUMBER,
+  v_pledgeamt IN NUMBER
+) RETURN NUMBER IS
+  v_monthly_payment NUMBER;
+BEGIN
+  v_monthly_payment := v_pledgeamt / v_paymonths;
+  RETURN v_monthly_payment;
+END DD_MTHPAY_SF;
+/
+
+-- Anonymous PL/SQL block demonstrating the use of the function
+DECLARE
+  v_pledge_amount          NUMBER := 240;
+  v_monthly_payments       NUMBER := 12;
+  v_monthly_payment_amount NUMBER;
+BEGIN
+  v_monthly_payment_amount := DD_MTHPAY_SF(v_monthly_payments, v_pledge_amount);
+  DBMS_OUTPUT.PUT_LINE('Monthly payment amount for the pledge: $'
+                       || v_monthly_payment_amount);
+END;
+/
+
+-- SQL statement to display information for all donor pledges in the database on a monthly payment plan
+SELECT
+  dp.idPledge,
+  dp.idDonor,
+  d.Firstname
+  || ' '
+  || d.Lastname                            AS Donor_Name,
+  dp.Pledgeamt                             AS Pledge_Amount,
+  dp.paymonths                             AS Payment_Months,
+  DD_MTHPAY_SF(dp.paymonths, dp.Pledgeamt) AS Monthly_Payment_Amount
+FROM
+  dd_pledge dp
+  JOIN dd_donor d
+  ON dp.idDonor = d.idDonor
+WHERE
+  dp.paymonths > 0;
 
 -- Assignment 6-10: Calculating the Total Project Pledge Amount
 -- Create a function named DD_PROJTOT_SF that determines the total pledge amount for a project.
 -- Use the function in an SQL statement that lists all projects, displaying project ID, project name,
 -- and project pledge total amount. Format the pledge total to display zero if no pledges have been
 -- made so far, and have it show a dollar sign, comma, and two decimal places for dollar values.
+CREATE OR REPLACE FUNCTION DD_PROJTOT_SF(
+  v_project_id IN NUMBER
+) RETURN NUMBER IS
+  v_total_pledge_amount NUMBER;
+BEGIN
+  SELECT
+    NVL(SUM(Pledgeamt), 0) INTO v_total_pledge_amount
+  FROM
+    dd_pledge
+  WHERE
+    idProj = v_project_id;
+  RETURN v_total_pledge_amount;
+END DD_PROJTOT_SF;
+/
+
+SELECT
+  idProj,
+  Projname,
+  TO_CHAR(DD_PROJTOT_SF(idProj), '$999,999,999.99') AS Project_Pledge_Total
+FROM
+  dd_project;
 
 -- Assignment 6-11: Identifying Pledge Status
 -- The DoGood Donor organization decided to reduce SQL join activity in its application by
--- Workspace.sql
+++ Workspace.sql
@@ -1,39 +1,40 @@
-CREATE OR REPLACE FUNCTION DD_MTHPAY_SF(
-  v_paymonths IN NUMBER,
-  v_pledgeamt IN NUMBER
-) RETURN NUMBER IS
-  v_monthly_payment NUMBER;
+
+CREATE OR REPLACE FUNCTION DD_PLSTAT_SF(
+  v_status_id IN NUMBER
+) RETURN VARCHAR2 IS
+  v_status_desc VARCHAR2(15);
 BEGIN
-  v_monthly_payment := v_pledgeamt / v_paymonths;
-  RETURN v_monthly_payment;
-END DD_MTHPAY_SF;
+  CASE v_status_id
+    WHEN 10 THEN
+      v_status_desc := 'Open';
+    WHEN 20 THEN
+      v_status_desc := 'Complete';
+    WHEN 30 THEN
+      v_status_desc := 'Overdue';
+    WHEN 40 THEN
+      v_status_desc := 'Closed';
+    WHEN 50 THEN
+      v_status_desc := 'Hold';
+    ELSE
+      v_status_desc := 'Unknown';
+  END CASE;
+
+  RETURN v_status_desc;
+END DD_PLSTAT_SF;
 /
 
--- Anonymous PL/SQL block demonstrating the use of the function
-DECLARE
-  v_pledge_amount          NUMBER := 240;
-  v_monthly_payments       NUMBER := 12;
-  v_monthly_payment_amount NUMBER;
-BEGIN
-  v_monthly_payment_amount := DD_MTHPAY_SF(v_monthly_payments, v_pledge_amount);
-  DBMS_OUTPUT.PUT_LINE('Monthly payment amount for the pledge: $'
-                       || v_monthly_payment_amount);
-END;
-/
-
--- SQL statement to display information for all donor pledges in the database on a monthly payment plan
 SELECT
-  dp.idPledge,
-  dp.idDonor,
-  d.Firstname
-  || ' '
-  || d.Lastname                            AS Donor_Name,
-  dp.Pledgeamt                             AS Pledge_Amount,
-  dp.paymonths                             AS Payment_Months,
-  DD_MTHPAY_SF(dp.paymonths, dp.Pledgeamt) AS Monthly_Payment_Amount
+  idPledge,
+  Pledgedate,
+  DD_PLSTAT_SF(idStatus) AS Pledge_Status
 FROM
-  dd_pledge dp
-  JOIN dd_donor d
-  ON dp.idDonor = d.idDonor
+  dd_pledge;
+
+SELECT
+  idPledge,
+  Pledgedate,
+  DD_PLSTAT_SF(idStatus) AS Pledge_Status
+FROM
+  dd_pledge
 WHERE
-  dp.paymonths > 0;
+  idPledge = :specified_pledge_id;
