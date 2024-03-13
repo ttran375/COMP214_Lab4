fa3fff90bd0ab6596007aedfe6c0abec9547c8ad
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-13 09:07:05 +0000

    Wed, Mar 13, 2024, 9:07 AM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,45 +1,52 @@
-CREATE OR REPLACE FUNCTION get_movie_stock(
-  movie_id_in NUMBER
-) RETURN VARCHAR2 IS
-  v_movie_title mm_movie.movie_title%TYPE;
-  v_movie_qty   mm_movie.movie_qty%TYPE;
-  v_message     VARCHAR2(100);
-BEGIN
- -- Retrieve movie title and quantity based on movie ID
-  SELECT
-    movie_title,
-    movie_qty INTO v_movie_title,
-    v_movie_qty
-  FROM
-    mm_movie
-  WHERE
-    movie_id = movie_id_in;
- -- Construct message
-  v_message := v_movie_title
-               || ' is ';
-  IF v_movie_qty > 0 THEN
-    v_message := v_message
-                 || 'Available: '
-                 || v_movie_qty
-                 || ' on the shelf.';
-  ELSE
-    v_message := v_message
-                 || 'Not available for rental at the moment.';
-  END IF;
- -- Return the formatted message
-  RETURN v_message;
-EXCEPTION
-  WHEN NO_DATA_FOUND THEN
-    RETURN 'Movie with ID '
-           || movie_id_in
-           || ' not found.';
+-- Assignment 7-1: Creating a Package
+-- Follow the steps to create a package containing a procedure and a function pertaining to basket
+-- information. (Note: The first time you compile the package body doesn’t give you practice with
+-- compilation error messages.)
+-- 1. Start Notepad, and open the Assignment07-01.txt file in the Chapter07 folder.
+-- 2. Review the package code, and then copy it.
+-- 3. In SQL Developer, paste the copied code to build the package.
+-- 4. Review the compilation errors and identify the related coding error.
+-- 5. Edit the package to correct the error and compile the package.
+CREATE OR REPLACE PACKAGE order_info_pkg IS
+ FUNCTION ship_name_pf  
+   (p_basket IN NUMBER)
+   RETURN VARCHAR2;
+ PROCEDURE basket_info_pp
+  (p_basket IN NUMBER,
+   p_shop OUT NUMBER,
+   p_date OUT DATE);
 END;
-/
 
--- Example usage:
-DECLARE
-  v_result VARCHAR2(100);
-BEGIN
-  v_result := get_movie_stock(3); -- Check stock for "Star Wars"
-  DBMS_OUTPUT.PUT_LINE(v_result);
+CREATE OR REPLACE PACKAGE BODY order_info_pkg IS
+ FUNCTION ship_name_pf  
+   (p_basket IN NUMBER)
+   RETURN VARCHAR2
+  IS
+   lv_name_txt VARCHAR2(25);
+ BEGIN
+  SELECT shipfirstname||' '||shiplastname
+   INTO lv_name_txt
+   FROM bb_basket
+   WHERE idBasket = p_basket;
+  RETURN lv_name_txt;
+ EXCEPTION
+   WHEN NO_DATA_FOUND THEN
+     DBMS_OUTPUT.PUT_LINE('Invalid basket id');
+ END ship_name_pf;
+ PROCEDURE basket_inf_pp
+  (p_basket IN NUMBER,
+   p_shop OUT NUMBER,
+   p_date OUT DATE)
+  IS
+ BEGIN
+   SELECT idshopper, dtordered
+    INTO p_shop, p_date
+    FROM bb_basket
+    WHERE idbasket = p_basket;
+ EXCEPTION
+   WHEN NO_DATA_FOUND THEN
+     DBMS_OUTPUT.PUT_LINE('Invalid basket id');
+ END basket_inf_pp;
 END;
+
+
