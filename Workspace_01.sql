24f6f914e4c532981e68d83773dc9fa0176618c4
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-19 09:12:55 +0000

    Tue, Mar 19, 2024, 9:12 AM +00:00

--- Workspace_01.sql
+++ Workspace_01.sql
@@ -7,19 +7,8 @@
 -- 3. In SQL Developer, paste the copied code to build the package.
 -- 4. Review the compilation errors and identify the related coding error.
 -- 5. Edit the package to correct the error and compile the package.
-CREATE OR REPLACE PACKAGE order_info_pkg IS
-  FUNCTION ship_name_pf (
-    p_basket IN NUMBER
-  ) RETURN VARCHAR2;
-  PROCEDURE basket_info_pp (
-    p_basket IN NUMBER,
-    p_shop OUT NUMBER,
-    p_date OUT DATE
-  );
-END;
-/
-
 CREATE OR REPLACE PACKAGE BODY order_info_pkg IS
+
   FUNCTION ship_name_pf (
     p_basket IN NUMBER
   ) RETURN VARCHAR2 IS
@@ -36,8 +25,9 @@ CREATE OR REPLACE PACKAGE BODY order_info_pkg IS
     RETURN lv_name_txt;
   EXCEPTION
     WHEN NO_DATA_FOUND THEN
-      DBMS_OUTPUT.PUT_LINE('Invalid basket id');
+      RAISE_APPLICATION_ERROR(-20001, 'Invalid basket id');
   END ship_name_pf;
+
   PROCEDURE basket_info_pp (
     p_basket IN NUMBER,
     p_shop OUT NUMBER,
@@ -46,14 +36,15 @@ CREATE OR REPLACE PACKAGE BODY order_info_pkg IS
   BEGIN
     SELECT
       idshopper,
-      dtordered INTO p_shop, p_date
+      dtordered INTO p_shop,
+      p_date
     FROM
       bb_basket
     WHERE
       idbasket = p_basket;
   EXCEPTION
     WHEN NO_DATA_FOUND THEN
-      DBMS_OUTPUT.PUT_LINE('Invalid basket id');
+      RAISE_APPLICATION_ERROR(-20002, 'Invalid basket id');
   END basket_info_pp;
 END;
 /
