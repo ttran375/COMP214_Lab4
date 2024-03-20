49948f68de4b0444e7fbf871e90948e7769fbf18
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-20 13:58:25 +0000

    Wed, Mar 20, 2024, 1:58 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,77 +1,86 @@
--- Assignment 9-7: Updating Summary Data Tables
--- The Brewbean’s owner uses several summary sales data tables every day to monitor business
--- activity. The BB_SALES_SUM table holds the product ID, total sales in dollars, and total
--- quantity sold for each product. A trigger is needed so that every time an order is confirmed or
--- the ORDERPLACED column is updated to 1, the BB_SALES_SUM table is updated
--- accordingly. Create a trigger named BB_SALESUM_TRG that performs this task. Before testing,
--- reset the ORDERPLACED column to 0 for basket 3, as shown in the following code, and use
--- this basket to test the trigger:
--- UPDATE bb_basket
--- SET orderplaced = 0
--- WHERE idBasket = 3;
--- Notice that the BB_SALES_SUM table already contains some data. Test the trigger with
--- the following UPDATE statement, and confirm that the trigger is working correctly:
--- UPDATE bb_basket
--- SET orderplaced = 1
--- WHERE idBasket = 3;
--- Do a rollback and disable the trigger when you’re finished so that it doesn’t affect other
+-- Assignment 9-8: Maintaining an Audit Trail of Product Table Changes
+-- The accuracy of product table data is critical, and the Brewbean’s owner wants to have an audit
+-- file containing information on all DML activity on the BB_PRODUCT table. This information
+-- should include the ID of the user performing the DML action, the date, the original values of the
+-- changed row, and the new values. This audit table needs to track specific columns of concern,
+-- including PRODUCTNAME, PRICE, SALESTART, SALEEND, and SALEPRICE. Create a table
+-- named BB_PRODCHG_AUDIT to hold the relevant data, and then create a trigger named
+-- BB_AUDIT_TRG that fires an update to this table whenever a specified column in the
+-- BB_PRODUCT table changes.
+-- TIP
+-- Multiple columns can be listed in a trigger’s OF clause by separating them with commas.
+-- Be sure to issue the following command. If you created the SALES_DATE_TRG trigger in the
+-- chapter, it conflicts with this assignment.
+-- ALTER TRIGGER sales_date_trg DISABLE;
+-- Use the following UPDATE statement to test the trigger:
+-- UPDATE bb_product
+-- SET salestart = '05-MAY-2012',
+-- saleend = '12-MAY-2012'
+-- saleprice = 9
+-- WHERE idProduct = 10;
+-- When you’re finished, do a rollback and disable the trigger so that it doesn’t affect other
 -- assignments.
-CREATE TABLE BB_SALES_SUM (
-  idProduct NUMBER(2),
-  TotalSales NUMBER(6, 2),
-  TotalQtySold NUMBER(5)
+
+CREATE TABLE BB_PRODCHG_AUDIT (
+  AUDIT_ID NUMBER(10) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  USER_ID VARCHAR2(30),
+  ACTION_DATE TIMESTAMP,
+  PRODUCT_ID NUMBER(10),
+  OLD_PRODUCTNAME VARCHAR2(255),
+  NEW_PRODUCTNAME VARCHAR2(255),
+  OLD_PRICE NUMBER,
+  NEW_PRICE NUMBER,
+  OLD_SALESTART DATE,
+  NEW_SALESTART DATE,
+  OLD_SALEEND DATE,
+  NEW_SALEEND DATE,
+  OLD_SALEPRICE NUMBER,
+  NEW_SALEPRICE NUMBER
 );
 
-ALTER TABLE BB_SALES_SUM ADD CONSTRAINT pk_bb_sales_sum PRIMARY KEY (idProduct);
-
-UPDATE bb_basket
-SET
-  orderplaced = 0
-WHERE
-  idBasket = 3;
-
-CREATE OR REPLACE TRIGGER BB_SALESUM_TRG AFTER
-  UPDATE OF orderplaced ON bb_basket FOR EACH ROW WHEN (NEW.orderplaced = 1)
+CREATE OR REPLACE TRIGGER BB_AUDIT_TRG AFTER
+  UPDATE OF PRODUCTNAME, PRICE, SALESTART, SALEEND, SALEPRICE ON BB_PRODUCT FOR EACH ROW
 BEGIN
-  FOR cur IN (
-    SELECT
-      idProduct,
-      SUM(Price * Quantity) AS TotalSales,
-      SUM(Quantity)         AS TotalQty
-    FROM
-      bb_basketItem
-    WHERE
-      idBasket = :NEW.idBasket
-    GROUP BY
-      idProduct
-  ) LOOP
-    UPDATE BB_SALES_SUM
-    SET
-      TotalSales = TotalSales + cur.TotalSales,
-      TotalQtySold = TotalQtySold + cur.TotalQty
-    WHERE
-      idProduct = cur.idProduct;
-    IF SQL%NOTFOUND THEN
-      INSERT INTO BB_SALES_SUM (
-        idProduct,
-        TotalSales,
-        TotalQtySold
-      ) VALUES (
-        cur.idProduct,
-        cur.TotalSales,
-        cur.TotalQty
-      );
-    END IF;
-  END LOOP;
+  INSERT INTO BB_PRODCHG_AUDIT (
+    USER_ID,
+    ACTION_DATE,
+    PRODUCT_ID,
+    OLD_PRODUCTNAME,
+    NEW_PRODUCTNAME,
+    OLD_PRICE,
+    NEW_PRICE,
+    OLD_SALESTART,
+    NEW_SALESTART,
+    OLD_SALEEND,
+    NEW_SALEEND,
+    OLD_SALEPRICE,
+    NEW_SALEPRICE
+  ) VALUES (
+    USER,
+    SYSTIMESTAMP,
+    :OLD.IDPRODUCT,
+    :OLD.PRODUCTNAME,
+    :NEW.PRODUCTNAME,
+    :OLD.PRICE,
+    :NEW.PRICE,
+    :OLD.SALESTART,
+    :NEW.SALESTART,
+    :OLD.SALEEND,
+    :NEW.SALEEND,
+    :OLD.SALEPRICE,
+    :NEW.SALEPRICE
+  );
 END;
 /
 
-UPDATE bb_basket
+UPDATE BB_PRODUCT
 SET
-  orderplaced = 1
+  SALESTART = '05-MAY-2012',
+  SALEEND = '12-MAY-2012',
+  SALEPRICE = 9
 WHERE
-  idBasket = 3;
+  IDPRODUCT = 10;
 
 ROLLBACK;
 
-ALTER TRIGGER BB_SALESUM_TRG DISABLE;
+ALTER TRIGGER BB_AUDIT_TRG DISABLE;
