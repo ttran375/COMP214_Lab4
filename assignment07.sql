a5b1ab7b65fdb0c9ff5bac31b84d0624e613f24d
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-25 08:36:38 +0000

    Mon, Mar 25, 2024, 8:36 AM +00:00

--- assignment07.sql
+++ assignment07.sql
@@ -386,7 +386,7 @@ END;
 -- to the same shopper, so they should return the same shopper information.
 
 CREATE OR REPLACE PACKAGE shop_query_pkg IS
-
+  -- Procedure to retrieve shopper information by shopper ID
   PROCEDURE retrieve_shopper (
     lv_id IN bb_shopper.idshopper%type,
     lv_name OUT VARCHAR,
@@ -395,7 +395,7 @@ CREATE OR REPLACE PACKAGE shop_query_pkg IS
     lv_phone OUT bb_shopper.phone%type,
     lv_email OUT bb_shopper.email%type
   );
-
+  -- Procedure to retrieve shopper information by last name
   PROCEDURE retrieve_shopper (
     lv_last IN bb_shopper.lastname%type,
     lv_name OUT VARCHAR,
@@ -408,7 +408,7 @@ END;
 /
 
 CREATE OR REPLACE PACKAGE BODY shop_query_pkg IS
-
+  -- Implementation of retrieve_shopper procedure by shopper ID
   PROCEDURE retrieve_shopper (
     lv_id IN bb_shopper.idshopper%type,
     lv_name OUT VARCHAR,
@@ -433,9 +433,8 @@ CREATE OR REPLACE PACKAGE BODY shop_query_pkg IS
     FROM
       bb_shopper
     WHERE
-      idshopper = lv_id;
-  END retrieve_shopper;
-
+      idshopper = lv_id; -- Filter by shopper ID
+  END retrieve_shopper; -- Implementation of retrieve_shopper procedure by last name
   PROCEDURE retrieve_shopper (
     lv_last IN bb_shopper.lastname%type,
     lv_name OUT VARCHAR,
@@ -460,13 +459,16 @@ CREATE OR REPLACE PACKAGE BODY shop_query_pkg IS
     FROM
       bb_shopper
     WHERE
-      lastname = lv_last;
+      lastname = lv_last; -- Filter by shopper's last name
   END retrieve_shopper;
 END;
 /
 
+-- Test the package twice
 DECLARE
+  -- Call the procedure with shopper ID 23
   lv_id    NUMBER := 23;
+  -- Call it with the last name Ratman
   lv_last  bb_shopper.lastname%type := 'Ratman';
   lv_name  VARCHAR2(25);
   lv_city  bb_shopper.city%type;
@@ -474,7 +476,9 @@ DECLARE
   lv_phone bb_shopper.phone%type;
   lv_email bb_shopper.email%type;
 BEGIN
+ -- Retrieve shopper information by shopper ID
   shop_query_pkg.retrieve_shopper(lv_id, lv_name, lv_city, lv_state, lv_phone, lv_email);
+ -- Display shopper information
   dbms_output.put_line(lv_name
                        ||' '
                        ||lv_city
@@ -484,7 +488,9 @@ BEGIN
                        ||lv_phone
                        ||' '
                        ||lv_email);
+ -- Retrieve shopper information by last name
   shop_query_pkg.retrieve_shopper(lv_last, lv_name, lv_city, lv_state, lv_phone, lv_email);
+ -- Display shopper information
   dbms_output.put_line(lv_name
                        ||' '
                        ||lv_city
