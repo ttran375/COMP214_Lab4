df282e6fb9d447f835c858773999efc2ac4dac79
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-25 08:47:32 +0000

    Mon, Mar 25, 2024, 8:47 AM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,95 +1,24 @@
-CREATE OR REPLACE PACKAGE BODY shop_query_pkg IS
- -- Implementation of retrieve_shopper procedure by shopper ID
-  PROCEDURE retrieve_shopper (
-    lv_id IN bb_shopper.idshopper%type,
-    lv_name OUT VARCHAR,
-    lv_city OUT bb_shopper.city%type,
-    lv_state OUT bb_shopper.state%type,
-    lv_phone OUT bb_shopper.phone%type,
-    lv_email OUT bb_shopper.email%type
-  ) IS
-  BEGIN
-    SELECT
-      firstname
-      ||' '
-      ||lastname,
-      city,
-      state,
-      phone,
-      email INTO lv_name,
-      lv_city,
-      lv_state,
-      lv_phone,
-      lv_email
-    FROM
-      bb_shopper
-    WHERE
-      idshopper = lv_id; -- Filter by shopper ID
-  END retrieve_shopper; -- Implementation of retrieve_shopper procedure by last name
-  PROCEDURE retrieve_shopper (
-    lv_last IN bb_shopper.lastname%type,
-    lv_name OUT VARCHAR,
-    lv_city OUT bb_shopper.city%type,
-    lv_state OUT bb_shopper.state%type,
-    lv_phone OUT bb_shopper.phone%type,
-    lv_email OUT bb_shopper.email%type
-  ) IS
-  BEGIN
-    SELECT
-      firstname
-      ||' '
-      ||lastname,
-      city,
-      state,
-      phone,
-      email INTO lv_name,
-      lv_city,
-      lv_state,
-      lv_phone,
-      lv_email
-    FROM
-      bb_shopper
-    WHERE
-      lastname = lv_last; -- Filter by shopper's last name
-  END retrieve_shopper;
+-- Assignment 7-6: Creating a Package with Only a Specification
+-- In this assignment, you create a package consisting of only a specification. The Brewbean’s
+-- lead programmer has noticed that only a few states require Internet sales tax, and the rates
+-- don’t change often. Create a package named TAX_RATE_PKG to hold the following tax rates in
+-- packaged variables for reference: pv_tax_nc = .035, pv_tax_tx = .05, and pv_tax_tn = .02.
+-- Code the variables to prevent the rates from being modified. Use an anonymous block with
+-- DBMS_OUTPUT statements to display the value of each packaged variable.
+
+-- Create a package consisting of only a specification
+CREATE OR REPLACE PACKAGE TAX_RATE_PKG IS
+  -- Hold the tax rates in packaged variables
+  pv_tax_nc CONSTANT NUMBER := .035; -- all variables are constants
+  pv_tax_tx CONSTANT NUMBER := .05;
+  pv_tax_tn CONSTANT NUMBER := .02;
 END;
 /
 
--- Test the package twice
-DECLARE
- -- Call the procedure with shopper ID 23
-  lv_id    NUMBER := 23;
- -- Call it with the last name Ratman
-  lv_last  bb_shopper.lastname%type := 'Ratman';
-  lv_name  VARCHAR2(25);
-  lv_city  bb_shopper.city%type;
-  lv_state bb_shopper.state%type;
-  lv_phone bb_shopper.phone%type;
-  lv_email bb_shopper.email%type;
+-- test our body-less package by printing the variables
 BEGIN
- -- Retrieve shopper information by shopper ID
-  shop_query_pkg.retrieve_shopper(lv_id, lv_name, lv_city, lv_state, lv_phone, lv_email);
- -- Display shopper information
-  dbms_output.put_line(lv_name
-                       ||' '
-                       ||lv_city
-                       ||' '
-                       ||lv_state
-                       ||' '
-                       ||lv_phone
-                       ||' '
-                       ||lv_email);
- -- Retrieve shopper information by last name
-  shop_query_pkg.retrieve_shopper(lv_last, lv_name, lv_city, lv_state, lv_phone, lv_email);
- -- Display shopper information
-  dbms_output.put_line(lv_name
-                       ||' '
-                       ||lv_city
-                       ||' '
-                       ||lv_state
-                       ||' '
-                       ||lv_phone
-                       ||' '
-                       ||lv_email);
+  DBMS_OUTPUT.PUT_LINE(TAX_RATE_PKG.pv_tax_nc);
+  DBMS_OUTPUT.PUT_LINE(TAX_RATE_PKG.pv_tax_tx);
+  DBMS_OUTPUT.PUT_LINE(TAX_RATE_PKG.pv_tax_tn);
 END;
 /
